70caa8c35c127cd85ff5dee6482fa41825d253bf
Jason Ahmed <jason_ahmed@yahoo.com>
2021-02-27 16:50:57 -0800

    working on styling navbar

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -5,22 +5,26 @@ import {Navbar,Nav,Container,Button,Form,FormControl} from 'react-bootstrap';
 const AppNavbar = () => {
 
   return (
-    <>
-    <Navbar bg="dark" variant="dark">
+  <>
+  <Navbar className="color-nav" variant="dark">
     <Navbar.Brand href="#home">
-            <Container fluid className='logo-image d-flex justify-content-center'>
-                <img src = {logo} width="50%" height="50%" alt="Logo"></img>
+            <Container fluid className='logo-image d-flex justify-content-left'>
+                <img src = {logo} width="80%" height="80%" alt="Logo"></img>
             </Container>
     </Navbar.Brand>
-    <Nav className="mr-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
+    <Container fluid className='d-flex justify-content-end'>
+    <Nav className="navButtons">
+      <Nav.Link href="#dashboard" className='myBtn text-center metal linear' style={{ color: 'black', textDecoration: 'none' }}>Dashboard</Nav.Link>
+      <Nav.Link href="#forum" className='myBtn text-center' style={{ color: 'black', textDecoration: 'none' }}>Forum</Nav.Link>
+      <Nav.Link href="#search-artists" className='myBtn text-center' style={{ color: 'black', textDecoration: 'none' }}>Search Artists</Nav.Link>
+      <Nav.Link href="#login-register"className='myBtn text-center' style={{ color: 'black', textDecoration: 'none' }}>Login/Register</Nav.Link>
     </Nav>
-    <Form inline>
+    </Container>
+    
+    {/* <Form inline>
       <FormControl type="text" placeholder="Search" className="mr-sm-2" />
       <Button variant="outline-info">Search</Button>
-    </Form>
+    </Form> */}
   </Navbar>
   </>
   );
