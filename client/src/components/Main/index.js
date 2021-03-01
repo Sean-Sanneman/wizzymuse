f85086065f9dc0e81516163c6384f01bac8743eb
Jason Ahmed <jason_ahmed@yahoo.com>
2021-03-01 14:39:04 -0800

    grid now displays properly

--- client/src/components/Main/index.js
+++ client/src/components/Main/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import coverImage from "../../assets/cover/cover-image-studio3.jpg";
+import {Container,Row,Col} from 'react-bootstrap';
 
 function Main() {
   return (
-    <section className="">
-      <h1 id="main">Main panel</h1>
-      <img src={coverImage} className="" style={{ width: "100%" }} alt="cover" />
-    </section>
+    <>
+    
+      <h1 id="main">About</h1>
+    
+
+      <Container fluid className="grid">
+      <Row className="mainGrid">
+        <Col className="leftPanel">1 of 3</Col>
+        <Col xs={8} className="midPanel">2 of 3 (wider)
+        <img src={coverImage} className="" style={{ width: "100%" }} alt="cover" />
+        </Col>
+        <Col className="rightPanel">3 of 3</Col>
+      </Row>
+      </Container>
+    
+      </>
   );
 }
 
