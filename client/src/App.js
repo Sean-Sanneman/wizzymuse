4c72e1e73b41952df04bfb80097e6bd38da0d584
Jason Ahmed <jason_ahmed@yahoo.com>
2021-03-01 16:00:20 -0800

    brushed metal background added

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import Nav from './components/Navbar';
 import Main from './components/Main';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import background from "./assets/cover/bg-brushed-metal.jpg";
 // import './App.css';
 
 function App() {
   return (
-    <div>
+    <div style={{ backgroundImage: `url(${background})` }}>
       <Nav></Nav>
       <main>
         <Main></Main>
