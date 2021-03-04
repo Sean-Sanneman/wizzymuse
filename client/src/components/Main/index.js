b3de2ba0bde111d3c7865b5cb5dc04973c69fc9a
Jason Ahmed <jason_ahmed@yahoo.com>
2021-03-03 17:07:41 -0800

    added left panel LED screen

--- client/src/components/Main/index.js
+++ client/src/components/Main/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import coverImage from "../../assets/cover/cover-image-studio3.jpg";
 import {Container,Row,Col} from 'react-bootstrap';
+import babyYoda from "../../assets/cover/baby-yoda.jpeg";
 
 function Main() {
   return (
     <>
       <Container fluid className="grid">
       <Row className="mainGrid">
-        <Col className="leftPanel allPanels">1 of 3</Col>
+        <Col className="leftPanel allPanels">1 of 3
+
+          <Container fluid="sm" className="leftLED">
+            <Row>
+              <Col className="LED-text"><h6>ARTIST_ID</h6>
+              <img src={babyYoda} className="babyYoda" style={{ width: "100%" }} alt="Baby Yoda" />
+              Instruments played: Guitar, Pan Flute
+              </Col>
+            </Row>
+          </Container>
+        
+        </Col>
         <Col xs={8} className="midPanel allPanels">2 of 3 (wider)
         <img src={coverImage} className="mainPic" style={{ width: "100%" }} alt="cover" />
         </Col>
