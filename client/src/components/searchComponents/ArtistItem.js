aac4a01e9dac33d339c833f28fc6c0fee46da93b
Jason Ahmed <jason_ahmed@yahoo.com>
2021-04-03 15:36:35 -0700

    added collapsible artist info btn

--- client/src/components/searchComponents/ArtistItem.js
+++ client/src/components/searchComponents/ArtistItem.js
@@ -1,13 +1,16 @@
 // React imports
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 // Redux imports
 // Components
 import Spinner from '../layoutComponents/Spinner';
 // Styles and Images
-import { Container, Row, Col, Card, Button } from 'react-bootstrap';
+import { Container, Row, Col, Card, Button, setOpen, Collapse } from 'react-bootstrap';
 import avatar1 from "../../assets/images/stock-avatar-1.jpeg";
 
 const ArtistItem = () => {
+
+    const [open, setOpen] = useState(false);
+
     return (
       <>
         <Card style={{ borderRadius: "15px" }} className="resultCards">
@@ -24,6 +27,26 @@ const ArtistItem = () => {
                             the card's content.
                         </Card.Text>
                         <Button variant="success">Collaborate</Button>
+
+                        {/* Collapse toggle button */}
+                        <Button className="collapseArtistItem" style={{ marginRight: "4%" }}
+                            onClick={() => setOpen(!open)}
+                            aria-controls="example-collapse-text"
+                            aria-expanded={open}
+                        >
+                        Artist Details
+                        </Button>
+                        <Collapse in={open}>
+                            <div id="example-collapse-text">
+                                <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus
+                                terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer
+                                labore wes anderson cred nesciunt sapiente ea proident.</p>
+                                <h4>The more artists' information that we add, graphics, whatever, the collapsible "box"
+                                    expands to allow it to fit. This may be useful, so that the initial artist "card" can
+                                    be smaller, then the profile opens up when you toggle.</h4>
+                            </div>
+                        </Collapse>
+
                 </Card.Body>
             </Col>
         </Row>
@@ -31,4 +54,5 @@ const ArtistItem = () => {
           </>
     );
   };
+
   export default ArtistItem;
