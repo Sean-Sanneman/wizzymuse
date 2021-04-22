220befc411962efb4c54d7e238f6a92962935796
Jason Ahmed <jason_ahmed@yahoo.com>
2021-04-22 15:50:22 -0700

    added placeholder ads

--- client/src/components/pages/Landing.js
+++ client/src/components/pages/Landing.js
@@ -14,6 +14,12 @@ import coverImage1 from '../../assets/cover/landing-image-1.jpg';
 import coverImage2 from '../../assets/cover/landing-image-2.jpg';
 import coverImage3 from '../../assets/cover/landing-image-3.jpg';
 import coverImage4 from '../../assets/cover/landing-image-4.jpg';
+import fakeAd1 from '../../assets/ads/fake-ad-1.jpg';
+import fakeAd2 from '../../assets/ads/fake-ad-2.jpg';
+import fakeAd3 from '../../assets/ads/fake-ad-3.jpg';
+import fakeAd4 from '../../assets/ads/fake-ad-4.jpg';
+import fakeAd5 from '../../assets/ads/fake-ad-5.jpg';
+
 import { Container, Row, Col, Carousel } from 'react-bootstrap';
 
 const Landing = ({ isAuthenticated }) => {
@@ -85,15 +91,57 @@ const Landing = ({ isAuthenticated }) => {
           </Carousel>
         </Col>
         <Col className="rightPanel allPanels">
-          <Container fluid="sm">
+          <Container fluid="sm" className="ads">
             <Row>
-              <Col className="LED-text">
-                <p>Advertisement 1</p>
-                <p>Advertisement 2</p>
-                <p>Advertisement 3</p>
-                <p>...</p>
-              </Col>
+              <Col> 
+                example ad              
+                <img
+                className="d-block w-100"
+                src={fakeAd1}
+                alt="fake ad 1"
+                />
+                </Col>
             </Row>
+            <Row>
+              <Col>
+                example ad               
+                <img
+                className="d-block w-100"
+                src={fakeAd4}
+                alt="fake ad 4"
+                />
+                </Col>
+            </Row>
+            {/* <Row>
+              <Col>
+                example ad             
+                <img
+                className="d-block w-100"
+                src={fakeAd5}
+                alt="fake ad 5"
+                />
+                </Col>
+            </Row> */}
+            <Row>
+              <Col>
+                example ad               
+                <img
+                className="d-block w-100"
+                src={fakeAd3}
+                alt="fake ad 3"
+                />
+                </Col>
+            </Row>
+            {/* <Row>
+              <Col>
+                example ad              
+                <img
+                className="d-block w-100"
+                src={fakeAd2}
+                alt="fake ad 2"
+                />
+                </Col>
+            </Row> */}
           </Container>
         </Col>
       </Row>
