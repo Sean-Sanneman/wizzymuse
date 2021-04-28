8e0ba1d0777bb27bab7812f3f4519014b55d8bff
Jason Ahmed <jason_ahmed@yahoo.com>
2021-04-28 16:41:51 -0700

    style for welcome text

--- client/src/components/landingComponents/WelText.js
+++ client/src/components/landingComponents/WelText.js
@@ -6,13 +6,25 @@ const Weltext = () => {
     return (
         <>
         
-                <h4>WizzyMuse is an online collaborative community. 
-                  <br></br>
-                  We believe that you can't put a price tag on the creative process, which is why our site will always be free to use for people looking to collaborate with others. 
-                  <br></br>
-                Your projects belong to you from the moment of inception. We utilize blockchain technology to ensure you feel confident that when you're ready to take your projects to the next level, everyone will know where your projects originated. Use whatever technology you're used to, and simply drop your tracks into your project page. Your collaborators will be able to drop their own tracks into your project, and drag them into place.
+                <h1>
+                  WizzyMuse is an online collaborative community.
+                </h1>
                 <br></br>
-                Our space allows you to put your collaboration together without worrying about technological issues. We all have our own ways we like to work, but that shouldn't stop us from putting the pieces together. Our community is about simplifying the process of collaborating remotely.</h4>
+                <h4>We believe that you can't put a price tag on the creative process, 
+                  which is why our site will always be free to use for people looking to collaborate with others.
+                </h4>
+                <br></br>
+                {/* <h4>Your projects belong to you from the moment of inception.
+                   We utilize blockchain technology to ensure you feel confident that when you're ready to take your projects to the next level. */}
+                    {/* everyone will know where your projects originated. */}
+                {/* </h4> */}
+                     {/* Use whatever technology you're used to, and simply drop your tracks into your project page. */}
+                     {/* Your collaborators will be able to drop their own tracks into your project, and drag them into place. */}
+                <br></br>
+                {/* <h3>Our space allows you to put your collaboration together without worrying about technological issues. */}
+                  {/* We all have our own ways we like to work, but that shouldn't stop us from putting the pieces together. */}
+                   {/* Our community is about simplifying the process of collaborating remotely. */}
+                {/* </h3> */}
                 </>
     )
 };
