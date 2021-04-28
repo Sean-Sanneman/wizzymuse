02baee70ba4fd5d1bd8847d1f1985d2f7a250a52
S. Sanneman <seandafydd@gmail.com>
2021-04-28 11:57:04 -0700

    fiddling with text

--- client/src/components/landingComponents/WelText.js
+++ client/src/components/landingComponents/WelText.js
@@ -5,18 +5,14 @@ import React from 'react';
 const Weltext = () => {
     return (
         <>
-        <h2>Welcome to <strong><em>Wizzy</em>Muse!</strong></h2>
-              <br></br>
-                <h5>WizzyMuse is an online collaborative community. 
-                  <br></br>
+        
+                <h4>WizzyMuse is an online collaborative community. 
                   <br></br>
                   We believe that you can't put a price tag on the creative process, which is why our site will always be free to use for people looking to collaborate with others. 
                   <br></br>
-                  <br></br>
                 Your projects belong to you from the moment of inception. We utilize blockchain technology to ensure you feel confident that when you're ready to take your projects to the next level, everyone will know where your projects originated. Use whatever technology you're used to, and simply drop your tracks into your project page. Your collaborators will be able to drop their own tracks into your project, and drag them into place.
                 <br></br>
-                <br></br>
-                Our space allows you to put your collaboration together without worrying about technological issues. We all have our own ways we like to work, but that shouldn't stop us from putting the pieces together. Our community is about simplifying the process of collaborating remotely.</h5>
+                Our space allows you to put your collaboration together without worrying about technological issues. We all have our own ways we like to work, but that shouldn't stop us from putting the pieces together. Our community is about simplifying the process of collaborating remotely.</h4>
                 </>
     )
 };
